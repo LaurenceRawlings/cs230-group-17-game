08fe1e064852727772abd3f15529f74c378ac82d
oscar3211 <58005047+oscar3211@users.noreply.github.com>
2019-12-08 19:29:49 +0000

    Javadoc

--- src/com/group17/game/core/Leaderboard.java
+++ src/com/group17/game/core/Leaderboard.java
@@ -1,3 +1,8 @@
+/**
+ * Class defines the leader board. This class contains the methods required
+ * to order profiles in accordance to the level reached and amount of time for completion.
+ * @author
+ */
 package com.group17.game.core;
 
 import java.util.ArrayList;
@@ -7,10 +12,22 @@ import java.util.Objects;
 
 public class Leaderboard {
     private static String compareLevel;
+    
+   /**
+    * Method to get the level to be compared.
+    * @return compareLevel
+    */
 
     public static String getCompareLevel() {
         return compareLevel;
     }
+    
+    /**
+	 * Method to get all of the top times.
+	 * @param levelName
+	 * @param amount
+	 * @return topProfiles
+	 */
 
     public static List<String> getTopTimes(String levelName, int amount) {
         compareLevel = levelName;
@@ -27,6 +44,12 @@ public class Leaderboard {
         }
         return topProfiles;
     }
+    
+    /**
+	 * Method to get the time calculated based off of time in seconds.
+	 * @param seconds
+	 * @return days, hours, minutes, seconds depending on the parameter amount
+	 */
 
     public static String formatTime(int seconds) {
         int minutes = seconds / 60;
@@ -45,6 +68,13 @@ public class Leaderboard {
         }
         return seconds + "s";
     }
+    
+    /**
+	 * Method to get the level name, profile name and time taken to complete the attempt
+	 * @param profileName
+	 * @param levelName
+	 * @return levelName and profileName that are not null
+	 */
 
     public static int getProfileTime(String profileName, String levelName) {
         return Objects.requireNonNull(ProfileManager.load(profileName)).getLevelTime(levelName);
