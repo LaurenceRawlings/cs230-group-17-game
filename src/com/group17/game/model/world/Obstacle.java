acfbc57b4f899bb2ce7efb47de6b3e7afc7f7f05
oscar3211 <58005047+oscar3211@users.noreply.github.com>
2019-12-08 21:11:54 +0000

    Javadoc

--- src/com/group17/game/model/world/Obstacle.java
+++ src/com/group17/game/model/world/Obstacle.java
@@ -1,14 +1,30 @@
+/**
+ * Class defines an obstacle cell.
+ * @author
+ */
+
 package com.group17.game.model.world;
 
 import com.group17.game.model.entity.item.Item;
 
 public abstract class Obstacle extends Cell {
     private final Item counterItem;
+    
+    /**
+     * Method sets the counter item
+     * @param counterItem
+     * @param spriteName
+     */
 
     Obstacle(Item counterItem, String spriteName) {
         super(false, spriteName);
         this.counterItem = counterItem;
     }
+    
+    /**
+     * Method to get the counter item
+     * @return counterItem
+     */
 
     public Item getCounterItem() {
         return counterItem;
