81f4b31ca7162bcaa5400deb7f63887fd8245368
oscar3211 <58005047+oscar3211@users.noreply.github.com>
2019-12-08 20:51:39 +0000

    Javadoc

--- src/com/group17/game/model/world/Cell.java
+++ src/com/group17/game/model/world/Cell.java
@@ -1,3 +1,8 @@
+/**
+ * Class representing a cell.
+ * @author
+ */
+
 package com.group17.game.model.world;
 
 import com.group17.game.model.GameObject;
@@ -5,10 +10,22 @@ import com.group17.game.model.GameObject;
 public abstract class Cell extends GameObject {
     private final boolean walkable;
 
+    /**
+     * Method allows cell to be walkable depending on the sprite
+     * @param walkable
+     * @param spriteName
+     */
+    
     Cell(boolean walkable, String spriteName) {
         super(spriteName);
         this.walkable = walkable;
     }
+    
+    /**
+     * Method to return if a sprite is walkable or not
+     * @return true or false
+     */
+    
     public boolean isWalkable (){
         return this.walkable;
     }
