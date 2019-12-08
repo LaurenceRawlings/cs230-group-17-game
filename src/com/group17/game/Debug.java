ae9918a760ed470479025bc1ec2c30e00e02b30b
Laurence Rawlings <rawlingslaurence@gmail.com>
2019-12-08 20:00:03 +0000

    Update master profiles.

--- src/com/group17/game/Debug.java
+++ src/com/group17/game/Debug.java
@@ -22,38 +22,38 @@ public class Debug {
 
         laurence.setLevelTime("Level 1", 60);
         vlad.setLevelTime("Level 1", 65);
-        hadi.setLevelTime("Level 1", 70);
-        tom.setLevelTime("Level 1", 75);
-        sam.setLevelTime("Level 1", 80);
-        oscar.setLevelTime("Level 1", 85);
+        hadi.setLevelTime("Level 1", 71);
+        tom.setLevelTime("Level 1", 76);
+        sam.setLevelTime("Level 1", 82);
+        oscar.setLevelTime("Level 1", 87);
 
-        laurence.setLevelTime("Level 2", 60);
-        vlad.setLevelTime("Level 2", 65);
-        hadi.setLevelTime("Level 2", 70);
-        tom.setLevelTime("Level 2", 75);
-        sam.setLevelTime("Level 2", 80);
-        oscar.setLevelTime("Level 2", 85);
+        laurence.setLevelTime("Level 2", 93);
+        vlad.setLevelTime("Level 2", 98);
+        hadi.setLevelTime("Level 2", 104);
+        tom.setLevelTime("Level 2", 109);
+        sam.setLevelTime("Level 2", 110);
+        oscar.setLevelTime("Level 2", 115);
 
-        laurence.setLevelTime("Level 3", 60);
-        vlad.setLevelTime("Level 3", 65);
-        hadi.setLevelTime("Level 3", 70);
-        tom.setLevelTime("Level 3", 75);
-        sam.setLevelTime("Level 3", 80);
-        oscar.setLevelTime("Level 3", 85);
+        laurence.setLevelTime("Level 3", 120);
+        vlad.setLevelTime("Level 3", 125);
+        hadi.setLevelTime("Level 3", 131);
+        tom.setLevelTime("Level 3", 136);
+        sam.setLevelTime("Level 3", 142);
+        oscar.setLevelTime("Level 3", 147);
 
-        laurence.setLevelTime("Level 4", 60);
-        vlad.setLevelTime("Level 4", 65);
-        hadi.setLevelTime("Level 4", 70);
-        tom.setLevelTime("Level 4", 75);
-        sam.setLevelTime("Level 4", 80);
-        oscar.setLevelTime("Level 4", 85);
+        laurence.setLevelTime("Level 4", 153);
+        vlad.setLevelTime("Level 4", 158);
+        hadi.setLevelTime("Level 4", 164);
+        tom.setLevelTime("Level 4", 169);
+        sam.setLevelTime("Level 4", 170);
+        oscar.setLevelTime("Level 4", 180);
 
-        laurence.setLevelTime("Level 5", 60);
-        vlad.setLevelTime("Level 5", 65);
-        hadi.setLevelTime("Level 5", 70);
-        tom.setLevelTime("Level 5", 75);
-        sam.setLevelTime("Level 5", 80);
-        oscar.setLevelTime("Level 5", 85);
+        laurence.setLevelTime("Level 5", 185);
+        vlad.setLevelTime("Level 5", 190);
+        hadi.setLevelTime("Level 5", 191);
+        tom.setLevelTime("Level 5", 206);
+        sam.setLevelTime("Level 5", 202);
+        oscar.setLevelTime("Level 5", 217);
 
         ProfileManager.save(laurence);
         ProfileManager.save(vlad);
