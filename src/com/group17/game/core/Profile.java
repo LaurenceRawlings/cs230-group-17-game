2aac074761ee3440d79617fd40a457e6f2758e26
oscar3211 <58005047+oscar3211@users.noreply.github.com>
2019-12-08 20:08:32 +0000

    Javadoc

--- src/com/group17/game/core/Profile.java
+++ src/com/group17/game/core/Profile.java
@@ -1,3 +1,8 @@
+/**
+ * Class defines the profile of a user. Determines highest level reached and time taken to complete.
+ * @author
+ */
+
 package com.group17.game.core;
 
 import java.io.Serializable;
@@ -9,14 +14,30 @@ public class Profile implements Serializable, Comparable<Profile> {
     private final String name;
     private final Map levelTimes;
     private int highestLevel;
+    
+    /**
+	 * Method to get the highest level reached by a user
+	 * @return highestLevel
+	 */
 
     public int getHighestLevel() {
         return highestLevel;
     }
+    
+    /**
+	 * Method to set the highest level reached by a user
+	 * @param highestLevel 
+	 */
 
     public void setHighestLevel(int highestLevel) {
         this.highestLevel = highestLevel;
     }
+    
+    /**
+	 * Method to get the time that it takes for a user to complete
+	 * @param levelName
+	 * @return the level times
+	 */
 
     public int getLevelTime(String levelName) {
         if (levelTimes.containsKey(levelName)) {
@@ -26,9 +47,20 @@ public class Profile implements Serializable, Comparable<Profile> {
         }
     }
 
+    /**
+	 * Method to check if a level is complete
+	 * @return true or false if complete or not
+	 */
+    
     public boolean levelCompleted(String levelName) {
         return levelTimes.containsKey(levelName);
     }
+    
+    /**
+	 * Method to set the time that it takes to complete a level
+	 * @param levelName
+	 * @param time
+	 */
 
     public void setLevelTime(String levelName, int time) {
         levelTimes.put(levelName, time);
@@ -49,15 +81,29 @@ public class Profile implements Serializable, Comparable<Profile> {
         levelTimes = new HashMap();
         highestLevel = -1;
     }
+    
+    /**
+	 * Method to set a new game
+	 */
 
     public void newGame() {
         game = new Game();
     }
+    
+    /**
+	 * Method to set a new game at a certain level
+	 * @param levelIndex
+	 */
 
     public void newGame(int levelIndex) {
         game = new Game(levelIndex);
     }
 
+    /**
+	 * Method to compare level reached to levels on the leader board
+	 * @param profile
+	 */
+    
     @Override
     public int compareTo(Profile profile) {
         String compareLevel = Leaderboard.getCompareLevel();
