2ce1056c0afeaf9890bfb9471fce8f217e290c1f
oscar3211 <58005047+oscar3211@users.noreply.github.com>
2019-12-08 21:15:41 +0000

    Javadoc

--- src/com/group17/game/model/world/Teleporter.java
+++ src/com/group17/game/model/world/Teleporter.java
@@ -1,3 +1,8 @@
+/**
+ * Class representing a teleporter
+ * @author
+ */
+
 package com.group17.game.model.world;
 
 import com.group17.game.core.Position;
@@ -5,6 +10,12 @@ import com.group17.game.core.Position;
 public class Teleporter extends Cell {
     private final Teleporter destination;
     private final Position position;
+    
+    /**
+     * Method sets the position and a new destination for where the player will be taken
+     * @param position
+     * @param destinationPosition
+     */
 
     public Teleporter(Position position, Position destinationPosition) {
         super(true, "Teleporter");
@@ -18,9 +29,19 @@ public class Teleporter extends Cell {
         this.destination = destination;
     }
 
+    /**
+     * Method gets the position of a teleporter
+     * @return position
+     */
+    
     public Position getPosition() {
         return position;
     }
+    
+    /**
+     * Method gets the destination of a teleporter
+     * @return destination
+     */
 
     public Teleporter getDestination() {
         return destination;
