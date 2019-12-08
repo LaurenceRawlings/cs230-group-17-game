9c051694fb6ebc7cf7389a3450e8295f45c27848
oscar3211 <58005047+oscar3211@users.noreply.github.com>
2019-12-08 20:59:19 +0000

    Javadoc

--- src/com/group17/game/model/world/KeyDoor.java
+++ src/com/group17/game/model/world/KeyDoor.java
@@ -1,14 +1,30 @@
+/**
+ * Class representing a key door.
+ * @author
+ */
+
 package com.group17.game.model.world;
 
 import com.group17.game.model.entity.item.Key;
 
 public abstract class KeyDoor extends Cell {
     private final Key.KeyType key;
+    
+    /**
+     * Method sets the key
+     * @param key
+     * @param spriteName
+     */
 
     KeyDoor(Key.KeyType key, String spriteName) {
         super(false, spriteName);
         this.key = key;
     }
+    
+    /**
+     * Method for getting the key
+     * @return key
+     */
 
     public Key.KeyType getKey() {
         return key;
